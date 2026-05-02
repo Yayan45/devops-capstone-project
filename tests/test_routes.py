340aeba3a232461fe400afc8aac83e18418ba592
unknown <yayanrifan45@gmail.com>
2026-05-02 17:46:13 +0700

    Fix final flake8 spacing issue

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,13 +1,17 @@
 import unittest
 from service import app
 
+
 class TestService(unittest.TestCase):
+
     def setUp(self):
         self.client = app.test_client()
 
     def test_get_accounts(self):
         resp = self.client.get("/accounts")
         self.assertEqual(resp.status_code, 200)
+
+
 
 if __name__ == "__main__":
     unittest.main()
